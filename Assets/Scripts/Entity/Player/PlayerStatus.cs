7282c1560a0b0eaa44f87db5c2fe644b42732399
KeichiMizutani <m.keichi16256@gmail.com>
2021-02-28 16:15:03 +0900

    playerStatusの利用

--- Assets/Scripts/Entity/Player/PlayerStatus.cs
+++ Assets/Scripts/Entity/Player/PlayerStatus.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerStatus : EntityStatus
 {
+    public float speed = 1.0f;
+    public float hp = 1000.0f;
+    public float attackBonusRatio = 1.3f;
+
     public override float Energy()
     {
         return base.Energy();
