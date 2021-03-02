3db4c4caaf1cb927df23a2129856120c39a4fd03
KeichiMizutani <m.keichi16256@gmail.com>
2021-03-03 00:43:22 +0900

    BulletPool

--- Assets/Scripts/Entity/Bullet/BulletController.cs
+++ Assets/Scripts/Entity/Bullet/BulletController.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialEnemyController : EntityController
+public class BulletController : EntityController
 {
+    
+
     public override void Start()
     {
-        entityStatus = new TutorialEnemyStatus();
         base.Start();
     }
 
     public override void Update()
     {
         base.Update();
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
     }
 
     public override void OnCollisionEnter(Collision other)
@@ -23,6 +29,5 @@ public class TutorialEnemyController : EntityController
     public override void AddDamage(float damage)
     {
         base.AddDamage(damage);
-        Debug.Log(damage.ToString("f2") + "のダメージを与えた！");
     }
 }
---
+++ Assets/Scripts/Entity/Bullet/BulletStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStatus : EntityStatus
+{
+    public override float Energy()
+    {
+        return base.Energy();   
+    }
+}
